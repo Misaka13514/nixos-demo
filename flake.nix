1e72300266be4e2d3666e7c2cbd8d6032fd5fd8e
Misaka13514 <Misaka13514@gmail.com>
2025-12-23 10:09:10 +0800

    style: format

--- flake.nix
+++ flake.nix
@@ -3,11 +3,13 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./configuration.nix
-      ];
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./configuration.nix
+        ];
+      };
     };
-  };
 }
